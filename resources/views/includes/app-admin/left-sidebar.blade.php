689758c670ea3cc7b41cc0f001ec034da6deafe9
Ray Andrew <13515073@std.stei.itb.ac.id>
2018-03-20 12:30:02 +0700

    add template

---
+++ resources/views/includes/app-admin/left-sidebar.blade.php
@@ -0,0 +1,37 @@
+<nav class="sidebar-nav">
+  <ul class="nav">
+    @guest
+      <li class="nav-title">
+        Users
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}"><i class="icon-login"></i> Login</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}"><i class="icon-user"></i> Register <span class="badge badge-primary">NEW</span></a>
+      </li>
+    @else
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}"><i class="icon-speedometer"></i> Dashboard <span class="badge badge-primary">NEW</span></a>
+      </li>
+      <li class="nav-title">
+        Components
+      </li>
+      <li class="nav-item nav-dropdown">
+        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-puzzle"></i> Base</a>
+        <ul class="nav-dropdown-items">
+          <li class="nav-item">
+            <a class="nav-link" href="base-breadcrumb.html"><i class="icon-puzzle"></i> Breadcrumb</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="base-cards.html"><i class="icon-puzzle"></i> Cards</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="base-carousel.html"><i class="icon-puzzle"></i> Carousel</a>
+          </li>
+        </ul>
+      </li>
+    @endguest
+  </ul>
+</nav>
+<button class="sidebar-minimizer brand-minimizer" type="button"></button>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix").setPublicPath("public");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +11,68 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+  .js("resources/assets/js/bootstrap.js", "public/js")
+  .js("resources/assets/js/app.js", "public/js")
+  .js("resources/assets/js/app-admin.js", "public/js")
+  .sass("resources/assets/sass/app.scss", "public/css")
+  .sass("resources/assets/sass/app-admin.scss", "public/css")
+  .copy("resources/assets/js/views/main.js", "public/js/views/main.js")
+  .copy("node_modules/pace-progress/pace.min.js", "public/js/pace.min.js")
+  .copy(
+    "node_modules/jquery.easing/jquery.easing.min.js",
+    "public/js/jquery.easing.min.js"
+  )
+  .copy(
+    "node_modules/waypoints/lib/jquery.waypoints.min.js",
+    "public/js/jquery.waypoints.min.js"
+  )
+  .copy(
+    "node_modules/owl.carousel/dist/owl.carousel.min.js",
+    "public/js/owl.carousel.min.js"
+  )
+  .copy(
+    "node_modules/magnific-popup/dist/jquery.magnific-popup.min.js",
+    "public/js/jquery.magnific-popup.min.js"
+  )
+  .copy(
+    "node_modules/timepicker/jquery.timepicker.min.js",
+    "public/js/jquery.timepicker.min.js"
+  )
+  .copy(
+    "node_modules/jquery.animate-number/jquery.animateNumber.min.js",
+    "public/js/jquery.animateNumber.min.js"
+  )
+  .copy(
+    "node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js",
+    "public/js/bootstrap-datepicker.min.js"
+  )
+  // styles copy
+  .copy(
+    "node_modules/owl.carousel/dist/assets/owl.carousel.min.css",
+    "public/css/owl.carousel.min.css"
+  )
+  .copy(
+    "node_modules/owl.carousel/dist/assets/owl.theme.default.min.css",
+    "public/css/owl.theme.default.min.css"
+  )
+  .copy(
+    "node_modules/magnific-popup/dist/magnific-popup.css",
+    "public/css/magnific-popup.css"
+  )
+  .copy(
+    "node_modules/timepicker/jquery.timepicker.min.css",
+    "public/css/jquery.timepicker.min.css"
+  )
+  .copy(
+    "node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css",
+    "public/css/bootstrap-datepicker.min.css"
+  )
+  .copy(
+    "node_modules/animate.css/animate.min.css",
+    "public/css/animate.min.css"
+  );
+
+if (mix.inProduction()) {
+  mix.version();
+}
